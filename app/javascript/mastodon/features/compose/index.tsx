4640eefc52aa54f21c06e15e87dd1bf6f5bbedd6
unarist <m.unarist@gmail.com>
2017-07-17 20:38:10 +0900

    Add announcements box in the compose area

--- app/javascript/mastodon/features/compose/index.tsx
+++ app/javascript/mastodon/features/compose/index.tsx
@@ -28,6 +28,7 @@ import { messages as navbarMessages } from '../ui/components/navigation_bar';
 
 import { Search } from './components/search';
 import ComposeFormContainer from './containers/compose_form_container';
+import Announcements from './components/announcements';
 
 const messages = defineMessages({
   live_feed_public: {
@@ -165,6 +166,7 @@ const Compose: React.FC<{ multiColumn: boolean }> = ({ multiColumn }) => {
         <div className='drawer__pager'>
           <div className='drawer__inner'>
             <ComposeFormContainer />
+            <Announcements />
 
             <div className='drawer__inner__mastodon'>
               <img alt='' draggable='false' src={mascot ?? elephantUIPlane} />
