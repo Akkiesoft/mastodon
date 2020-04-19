eacc9cc76391cbd00a7705ac4e80dc15112d76ce
Akira Ouchi <akkiesoft@marokun.net>
2020-04-19 11:32:28 +0900

    rename Announcement names to UnaristAnnouncements
    reference: https://github.com/Na0ki/mastodon/commit/7d056622ac2c9314708969c19344b1bf448ff9a2

--- app/javascript/mastodon/features/ui/components/compose_panel.jsx
+++ app/javascript/mastodon/features/ui/components/compose_panel.jsx
@@ -10,6 +10,7 @@ import SearchContainer from 'mastodon/features/compose/containers/search_contain
 import { identityContextPropShape, withIdentity } from 'mastodon/identity_context';
 
 import LinkFooter from './link_footer';
+import UnaristAnnouncements from 'mastodon/features/compose/components/announcements';
 
 class ComposePanel extends PureComponent {
   static propTypes = {
@@ -53,6 +54,7 @@ class ComposePanel extends PureComponent {
 
         {signedIn && (
           <ComposeFormContainer singleColumn />
+          <UnaristAnnouncements />
         )}
 
         <LinkFooter />
